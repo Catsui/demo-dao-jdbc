91683a45529f43891c7e5d09358cbbbd994b8649
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2019-09-02 14:49:32 -0300

    Implementação update

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,12 +31,19 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);;		
 		
-		System.out.println("\n=== TEST 3: seller insert ===");
+		System.out.println("\n=== TEST 4: seller insert ===");
 
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000., department);
 		
 		sellerDao.insert(newSeller);
 		System.out.println("Vendedor inserido. Id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completo");
 	}
 
 }
